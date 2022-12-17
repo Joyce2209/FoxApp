ca103021defcec410489d11756fbdbd806f4e5e6
Joyce2209 <joycee.ruffino@gmail.com>
2022-12-17 17:00:37 -0300

    FoxApp

---
+++ app/src/main/java/br/com/etecia/hotelaps/reservaActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.hotelaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reservaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reserva_layout);
+    }
+}
